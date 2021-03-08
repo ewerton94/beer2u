54719cbf041f5d4336a0509e09508bac721f7511
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-07 21:28:32 -0300

    feat: New fields #1

--- frontend/src/app/dashboard/filtro-box/filtro-box.component.ts
+++ frontend/src/app/dashboard/filtro-box/filtro-box.component.ts
@@ -1,12 +1,46 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import {MatAccordion} from '@angular/material/expansion';
+
+export interface City {
+  name: string;
+}
+
+export interface State {
+  name: string;
+}
+export interface IQueryFilter {
+  city: string;
+  state: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-filtro-box',
   templateUrl: './filtro-box.component.html',
   styleUrls: ['./filtro-box.component.css']
 })
+
 export class FiltroBoxComponent {
   @ViewChild(MatAccordion) accordion: MatAccordion;
 
+  cities: City[] = [
+    {name: 'Maceió'},
+    {name: 'São Paulo'}
+  ]
+  
+  states: State[] = [
+    {name: 'Alagoas'},
+    {name: 'São Paulo'}
+  ]
+
+  queryForm = new FormGroup({
+    city: new FormControl({}),
+    state: new FormControl({}),
+    date: new FormControl('')
+  })
+
+  onSubmit() {
+    window.alert('haha')
+  }
 }
