9d9022ba085640a2d33b158f29028875240c3278
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-09 00:22:46 -0300

    feat: States endpoint #7

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from db import Mongo
+import cherrypy
+
+from states import States
+from cities import Cities
+
+
+if __name__ == '__main__':
+    m = Mongo()
+    conf = {
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+            'tools.auth_digest.accept_charset': 'UTF-8',
+            'tools.encode.encoding': 'utf-8'
+        }
+    }
+    cherrypy.tree.mount(States(m), '/api/states/', conf)
+    cherrypy.quickstart(Cities(m), '/api/cities/', conf)
---
+++ backend/states.py
@@ -0,0 +1,13 @@
+from utils import BaseAPI
+
+
+class States(BaseAPI):
+
+    def __init__(self, db):
+        self.db = db
+
+    def getStates(self):
+        return list(self.db.get_col('states').find())
+
+    def GET(self):
+        return self.response(self.getStates())
