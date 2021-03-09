9ef2e7a3440debff5f3ce3ea63bd38413b9638b2
Ewerton Oliveira <ewerton.amorim@live.com>
2021-03-09 00:23:33 -0300

    feat: Create cities endpoint #8

---
+++ backend/cities.py
@@ -0,0 +1,13 @@
+from utils import BaseAPI
+
+
+class Cities(BaseAPI):
+
+    def __init__(self, db):
+        self.db = db
+
+    def getCities(self, state):
+        return list(self.db.get_col('cities').find({'state': state}))
+
+    def GET(self, state='AL'):
+        return self.response(self.getCities(state))
